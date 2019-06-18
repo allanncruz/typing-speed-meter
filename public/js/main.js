b7fc4d3321dc5b36bfcf17c965bb7837576bb91a
Allan Cruz <contato@allancruz.com.br>
2019-06-18 00:03:18 -0300

    JQuery Events

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
-var phrase = $(".phrase").text();
-var numberWords = phrase.split(" ").length;
-var phraseSize = $("#phraseSize");
+ var phrase = $(".phrase").text();
+ var numberWords = phrase.split(" ").length;
+ var phraseSize = $("#phraseSize");
 
 phraseSize.text(numberWords);
+
+var field = $(".typing-field");
+field.on("input", function(){
+    var content = field.val();
+
+    var numberWords = content.split(/\S+/).length - 1;
+    $("#counterWords").text(numberWords);
+    console.log(content.length);
+
+    var counterCharacters = content.length;
+    $("#counterCharacters").text(counterCharacters);
+});
