6b83a7a48aba596b212c05140ab96c8b4a18e4b5
allanncruz <contato@allancruz.com.br>
2019-09-09 20:25:03 -0300

    Restarting the game

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,53 @@
- var phrase = $(".phrase").text();
- var numberWords = phrase.split(" ").length;
- var phraseSize = $("#phraseSize");
-
-phraseSize.text(numberWords);
-
+var StartTime = $("#typing-time").text();
 var field = $(".typing-field");
-field.on("input", function(){
-    var content = field.val();
 
-    var numberWords = content.split(/\S+/).length - 1;
-    $("#counterWords").text(numberWords);
-    console.log(content.length);
-
-    var counterCharacters = content.length;
-    $("#counterCharacters").text(counterCharacters);
+$(function(){
+    initializeCounters();
+    updateSentenceSize();
+    initializeTimer();
+    $("#start-button").click(restartGame);
 });
 
+function updateSentenceSize(){
+    var phrase = $(".phrase").text();
+    var numberWords = phrase.split(" ").length;
+    var phraseSize = $("#phraseSize");
+    phraseSize.text(numberWords);
+}
 
-var typingTime = $("#typing-time").text();
-field.one("focus", function(){
-   var cronometroID =  setInterval(function(){
-       typingTime--;
-       $("#typing-time").text(typingTime);
-       if (typingTime < 1) {
-           field.attr("disabled", true);
-           clearInterval(cronometroID);
-       }
-   }, 1000)
-});
+function initializeCounters() {
+    field.on("input", function(){
+        var content = field.val();
+    
+        var numberWords = content.split(/\S+/).length - 1;
+        $("#counterWords").text(numberWords);
+        console.log(content.length);
+    
+        var counterCharacters = content.length;
+        $("#counterCharacters").text(counterCharacters);
+    });
+}
+
+function initializeTimer(){
+    var typingTime = $("#typing-time").text();
+    field.one("focus", function(){
+        var cronometroID =  setInterval(function(){
+            typingTime--;
+            $("#typing-time").text(typingTime);
+            if (typingTime < 1) {
+                field.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        }, 1000)
+    });
+}
+
+function restartGame(){
+    field.attr("disabled", false);
+    field.val("");
+    $("#counterWords").text("0");
+    $("#counterCharacters").text("0");
+    $("#typing-time").text(StartTime);
+    initializeTimer();
+}
+
