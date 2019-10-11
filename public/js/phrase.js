3cd30554d4a1b2bec0f32cb8dfc40fe6d5852a30
allanncruz <contato@allancruz.com.br>
2019-10-10 23:43:11 -0300

    Implementing Spinner

--- public/js/phrase.js
+++ public/js/phrase.js
@@ -1,12 +1,16 @@
 $("#buttom-phrase").click(randomPhrase);
 
 function randomPhrase() {
+    $("#spinner").toggle(); //mostrando o spinner
     $.get("http://localhost:3000/frases",changePhrase)
     .fail(function(){
         $("#erro").toggle();
         setTimeout(function(){
             $("#erro").toggle();
         },1500);
+    })
+    .always(function(){ //sempre escondendo o spinner
+        $("#spinner").toggle();
     });
 }
 
